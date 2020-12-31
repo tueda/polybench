cb1452f9981d74c5200e26257b8eee6f9817bb4d
Takahiro Ueda <tueda@st.seikei.ac.jp>
2020-12-31 17:24:44 +0900

    feat: add polybench.__version__

--- polybench/__init__.py
+++ polybench/__init__.py
@@ -1,1 +1,5 @@
 """Multivariate polynomial arithmetic benchmark tests."""
+
+from .version import __version__
+
+__all__ = ("__version__",)
---
+++ polybench/version.py
@@ -0,0 +1,20 @@
+"""Version information."""
+
+from pathlib import Path
+from typing import cast
+
+import importlib_metadata
+import toml
+
+
+def _get_version(pyproject_toml: Path) -> str:
+    data = toml.load(pyproject_toml)
+    return cast(str, data["tool"]["poetry"]["version"])
+
+
+try:
+    # This is valid only if the package has been installed.
+    __version__ = cast(str, importlib_metadata.version(__name__))
+except importlib_metadata.PackageNotFoundError:
+    # The package is not installed. Assume that there is pyproject.toml.
+    __version__ = _get_version(Path(__file__).parent.parent / "pyproject.toml")
