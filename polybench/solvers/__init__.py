bf79425fd7433e45fa17ae8ce8f44582b099c846
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-06-18 15:54:38 +0900

    chore: update pre-commit config

--- polybench/solvers/__init__.py
+++ polybench/solvers/__init__.py
@@ -6,7 +6,7 @@ from pkgutil import iter_modules
 
 # Load all modules.
 
-for (_, module_name, _) in iter_modules(  # type: ignore[assignment]
+for _, module_name, _ in iter_modules(  # type: ignore[assignment]
     [str(Path(__file__).resolve().parent)]
 ):
     import_module(f"{__name__}.{module_name}")
