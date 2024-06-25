ae9e2c26248af5b046931d1776c62425eb0b2734
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-06-25 11:19:36 +0900

    test: change nvars

--- scripts/benchmark/0.3.0.sh
+++ scripts/benchmark/0.3.0.sh
@@ -13,18 +13,88 @@ if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
 fi
 
 run030() {
+  local nvars
+  nvars=$1
+  shift
   run020 \
+    --output-directory "$result_dir/$output_dir$suffix-$nvars" \
+    --nvars $nvars
     --nproblems 200 \
     --max-nterms 50 \
     "$@"
 }
 
-run030 --type trivial-gcd
-run030 --type nontrivial-gcd
-run030 --type trivial-factor
-run030 --type nontrivial-factor
+run030 1 --type trivial-gcd --exp-dist uniform
+run030 1 --type nontrivial-gcd --exp-dist uniform
+run030 1 --type trivial-factor --exp-dist uniform
+run030 1 --type nontrivial-factor --exp-dist uniform
 
-run030 --type trivial-gcd --exp-dist uniform
-run030 --type nontrivial-gcd --exp-dist uniform
-run030 --type trivial-factor --exp-dist uniform
-run030 --type nontrivial-factor --exp-dist uniform
+run030 2 --type trivial-gcd --exp-dist uniform
+run030 2 --type nontrivial-gcd --exp-dist uniform
+run030 2 --type trivial-factor --exp-dist uniform
+run030 2 --type nontrivial-factor --exp-dist uniform
+
+run030 2 --type trivial-gcd --exp-dist sharp
+run030 2 --type nontrivial-gcd --exp-dist sharp
+run030 2 --type trivial-factor --exp-dist sharp
+run030 2 --type nontrivial-factor --exp-dist sharp
+
+run030 3 --type trivial-gcd --exp-dist uniform
+run030 3 --type nontrivial-gcd --exp-dist uniform
+run030 3 --type trivial-factor --exp-dist uniform
+run030 3 --type nontrivial-factor --exp-dist uniform
+
+run030 3 --type trivial-gcd --exp-dist sharp
+run030 3 --type nontrivial-gcd --exp-dist sharp
+run030 3 --type trivial-factor --exp-dist sharp
+run030 3 --type nontrivial-factor --exp-dist sharp
+
+run030 4 --type trivial-gcd --exp-dist uniform
+run030 4 --type nontrivial-gcd --exp-dist uniform
+run030 4 --type trivial-factor --exp-dist uniform
+run030 4 --type nontrivial-factor --exp-dist uniform
+
+run030 4 --type trivial-gcd --exp-dist sharp
+run030 4 --type nontrivial-gcd --exp-dist sharp
+run030 4 --type trivial-factor --exp-dist sharp
+run030 4 --type nontrivial-factor --exp-dist sharp
+
+run030 5 --type trivial-gcd --exp-dist uniform
+run030 5 --type nontrivial-gcd --exp-dist uniform
+run030 5 --type trivial-factor --exp-dist uniform
+run030 5 --type nontrivial-factor --exp-dist uniform
+
+run030 5 --type trivial-gcd --exp-dist sharp
+run030 5 --type nontrivial-gcd --exp-dist sharp
+run030 5 --type trivial-factor --exp-dist sharp
+run030 5 --type nontrivial-factor --exp-dist sharp
+
+run030 6 --type trivial-gcd --exp-dist uniform
+run030 6 --type nontrivial-gcd --exp-dist uniform
+run030 6 --type trivial-factor --exp-dist uniform
+run030 6 --type nontrivial-factor --exp-dist uniform
+
+run030 6 --type trivial-gcd --exp-dist sharp
+run030 6 --type nontrivial-gcd --exp-dist sharp
+run030 6 --type trivial-factor --exp-dist sharp
+run030 6 --type nontrivial-factor --exp-dist sharp
+
+run030 7 --type trivial-gcd --exp-dist uniform
+run030 7 --type nontrivial-gcd --exp-dist uniform
+run030 7 --type trivial-factor --exp-dist uniform
+run030 7 --type nontrivial-factor --exp-dist uniform
+
+run030 7 --type trivial-gcd --exp-dist sharp
+run030 7 --type nontrivial-gcd --exp-dist sharp
+run030 7 --type trivial-factor --exp-dist sharp
+run030 7 --type nontrivial-factor --exp-dist sharp
+
+run030 8 --type trivial-gcd --exp-dist uniform
+run030 8 --type nontrivial-gcd --exp-dist uniform
+run030 8 --type trivial-factor --exp-dist uniform
+run030 8 --type nontrivial-factor --exp-dist uniform
+
+run030 8 --type trivial-gcd --exp-dist sharp
+run030 8 --type nontrivial-gcd --exp-dist sharp
+run030 8 --type trivial-factor --exp-dist sharp
+run030 8 --type nontrivial-factor --exp-dist sharp
